44a87299dcc4042f7c4911829c02e5b50496d382
neckbor <neckbor@mail.ru>
2023-05-16 22:43:43 +0400

    - added Triangle class that inherits Figure;

---
+++ TestTask/FigureLib/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLib
+{
+    public class Triangle : Figure
+    {
+        private double _a;
+        private double _b;
+        private double _c;
+
+        public Triangle(double a, double b, double c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

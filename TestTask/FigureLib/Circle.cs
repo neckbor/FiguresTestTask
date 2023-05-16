506e99542f91c20addcd044f69bb96787171b81d
neckbor <neckbor@mail.ru>
2023-05-16 22:38:34 +0400

    - added abstract Figure class and Circle class that inherits Figure;

---
+++ TestTask/FigureLib/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureLib
+{
+    public class Circle : Figure
+    {
+        private double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

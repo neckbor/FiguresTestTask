7e92cb3d7f4fb4025b338aa31003b7401538617f
neckbor <neckbor@mail.ru>
2023-05-16 22:41:59 +0400

    - radius validation in constructor; - Area method implementation;

--- TestTask/FigureLib/Circle.cs
+++ TestTask/FigureLib/Circle.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FigureLib
+﻿namespace FigureLib
 {
     public class Circle : Figure
     {
@@ -12,12 +6,17 @@ namespace FigureLib
 
         public Circle(double radius)
         {
+            if (radius < 0)
+            {
+                throw new ArgumentException("Сircle raduis can't be less than zero");
+            }
+
             _radius = radius;
         }
 
         public override double GetArea()
         {
-            throw new NotImplementedException();
+            return Math.PI * Math.Pow(_radius, 2);
         }
     }
 }
